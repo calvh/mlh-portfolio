34cbb1bd6a213a450aab81d0f1ae1bf50829f8ae
coffeemates <jrosa1756@gmail.com>
2021-06-08 11:23:15 -0500

    Added different content based on URL

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ load_dotenv()
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+@app.route('/<person>')
+def index(person):
+    profile_picture = {'Josue': './static/img/miyabi.jpg',
+                 'Carolina': './static/img/logo.jpg',
+                 'Calvin': './static/img/logo.jpg'}
+    description = {'Josue': 'Howdy I\'m Josue from Texas',
+                 'Carolina': 'Hello I\'m Caro from Mexico',
+                 'Calvin': 'Hellow I\'m Cal from Canada'}
+    return render_template('index.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
