070e5807f9104456791d6963842b2d633dca6112
coffeemates <jrosa1756@gmail.com>
2021-06-08 14:08:55 -0500

    Added a default case, will be used for testing

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
+@app.route('/', defaults={'person':'Miyabi'})
 @app.route('/<person>')
 def index(person):
-    profile_picture = {'Josue': './static/img/miyabi.jpg',
+    profile_picture = {'Miyabi': './static/img/miyabi.jpg',
+                'Josue': './static/img/miyabi.jpg',
                  'Carolina': './static/img/logo.jpg',
                  'Calvin': './static/img/logo.jpg'}
-    description = {'Josue': 'Howdy I\'m Josue from Texas',
+    description = {'Miyabi': 'Hi I\'m Miyabi Serizawa from the manga Domestic Girlfriend',
+                'Josue': 'Howdy I\'m Josue from Texas',
                  'Carolina': 'Hello I\'m Caro from Mexico',
                  'Calvin': 'Hellow I\'m Cal from Canada'}
     return render_template('index.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
