643e4353cfcba425ab27f2b42a3c09048bd5ceb0
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-06-09 15:22:10 -0400

    Fix render functions in __init__.py
    Rename index.html to base.html
    Modified layout of base.html
    Add index.html as template for the About page
    Add templates for contact, resume, projects pages
    Fix CSS selector for ul tags

--- app/__init__.py
+++ app/__init__.py
@@ -7,37 +7,68 @@ load_dotenv()
 app = Flask(__name__)
 nav = Navigation(app)
 
-nav.Bar('top', [
-    nav.Item('About Me', 'index'),
-    nav.Item('Projects', 'projects'),
-    nav.Item('Contact Me', 'contact'),
-    nav.Item('Resume', 'resume')])
+nav.Bar(
+    "top",
+    [
+        nav.Item("About Me", "index"),
+        nav.Item("Projects", "projects"),
+        nav.Item("Contact Me", "contact"),
+        nav.Item("Resume", "resume"),
+    ],
+)
 
-@app.route('/')
-@app.route('/about')
+
+@app.route("/")
 def index():
-    person = 'Miyabi'
-    profile_picture = './static/img/miyabi.jpg'
-    description = 'about me'
-    return render_template('index.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
+    title = "Miyabi"
+    profile_picture = "./static/img/miyabi.jpg"
+    description = "about me"
+    return render_template(
+        "index.html",
+        title=title,
+        profile_picture=profile_picture,
+        description=description,
+        url=os.getenv("URL"),
+    )
 
-@app.route('/projects')
+
+@app.route("/projects")
 def projects():
-    title = 'Projects'
-    profile_picture = './static/img/miyabi.jpg'
-    description = 'Project'
-    return render_template('projects.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
+    title = "Projects"
+    profile_picture = "./static/img/miyabi.jpg"
+    description = "Project"
+    return render_template(
+        "projects.html",
+        title=title,
+        profile_picture=profile_picture,
+        description=description,
+        url=os.getenv("URL"),
+    )
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    person = 'Contact'
-    profile_picture = './static/img/miyabi.jpg'
-    description = 'contact'
-    return render_template('contact.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
+    title = "Contact"
+    profile_picture = "./static/img/miyabi.jpg"
+    description = "contact"
+    return render_template(
+        "contact.html",
+        title=title,
+        profile_picture=profile_picture,
+        description=description,
+        url=os.getenv("URL"),
+    )
 
-@app.route('/resume')
+
+@app.route("/resume")
 def resume():
-    person = 'Resume'
-    profile_picture = './static/img/miyabi.jpg'
-    description = 'Resume'
-    return render_template('resume.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
+    title = "Resume"
+    profile_picture = "./static/img/miyabi.jpg"
+    description = "Resume"
+    return render_template(
+        "resume.html",
+        title=title,
+        profile_picture=profile_picture,
+        description=description,
+        url=os.getenv("URL"),
+    )
