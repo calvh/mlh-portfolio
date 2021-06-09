47eff2577b4d5b171a9bfec332759e2d81d9daa4
coffeemates <jrosa1756@gmail.com>
2021-06-08 20:30:56 -0500

    Added nav bar stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask, render_template, send_from_directory
+from flask_navigation import Navigation
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
+nav = Navigation(app)
+
+nav.Bar('top', [
+    nav.Item('Calvin', 'index', {'person': 'Calvin'}),
+    nav.Item('Josue', 'index', {'person': 'Josue'}),
+    nav.Item('Carolina', 'index', {'person': 'Carolina'})])
 
 @app.route('/', defaults={'person':'Miyabi'})
 @app.route('/<person>')
