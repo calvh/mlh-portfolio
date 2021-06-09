fea4f88c30196d4de5aec664614a7858e87463c4
coffeemates <jrosa1756@gmail.com>
2021-06-09 11:20:53 -0500

    Added 404 Page, with proper response

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ load_dotenv()
 app = Flask(__name__)
 nav = Navigation(app)
 
+<<<<<<< HEAD
 nav.Bar(
     "top",
     [
@@ -72,3 +73,26 @@ def resume():
         description=description,
         url=os.getenv("URL"),
     )
+=======
+nav.Bar('top', [
+    nav.Item('Calvin', 'index', {'person': 'Calvin'}),
+    nav.Item('Josue', 'index', {'person': 'Josue'}),
+    nav.Item('Carolina', 'index', {'person': 'Carolina'})])
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html', url=os.getenv("URL"))
+
+@app.route('/<person>')
+def index(person):
+    profile_picture = {'Miyabi': './static/img/miyabi.jpg',
+                'Josue': './static/img/miyabi.jpg',
+                 'Carolina': './static/img/logo.jpg',
+                 'Calvin': './static/img/logo.jpg'}
+    description = {'Miyabi': 'Hi I\'m Miyabi Serizawa from the manga Domestic Girlfriend',
+                'Josue': 'Howdy I\'m Josue from Texas',
+                 'Carolina': 'Hello I\'m Caro from Mexico',
+                 'Calvin': 'Hellow I\'m Cal from Canada'}
+    return render_template('index.html', title=person, profile_picture=profile_picture , description=description, url=os.getenv("URL"))
+>>>>>>> c2802f7 (Added 404 Page, with proper response)
