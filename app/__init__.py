5efa00eb1005366251430adb442d34e91652091a
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-06-11 15:30:27 -0400

    Add wsgi, whitenoise, Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
+from whitenoise import WhiteNoise
+
 from flask import Flask
 from flask import render_template
 
 from app.portfolio import portfolio
 
 app = Flask(__name__)
+
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
 app.register_blueprint(portfolio)
 
